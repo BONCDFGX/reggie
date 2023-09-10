ea8c57675b7cb9a3ffcfc7d0266d17c5d4228c0f
bonc <boncdfgx@outlook.com>
2023-09-10 08:24:43 +0800

    2023.09.10-4 搭建mapper，service，serviceImpl，controller三层MVC架构，并编写Employee的三层接口方法

---
+++ src/main/java/com/bonc/reggie/service/EmployeeService.java
@@ -0,0 +1,7 @@
+package com.bonc.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.bonc.reggie.entity.Employee;
+
+public interface EmployeeService extends IService<Employee>{
+}

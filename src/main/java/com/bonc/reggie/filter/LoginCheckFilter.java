34649505da852aaec57fc2e229536aff61eaee87
bonc <boncdfgx@outlook.com>
2023-09-12 23:10:07 +0800

    2023.09.12-10 完善LoginCheckFilter过滤器，实现检查登录功能

--- src/main/java/com/bonc/reggie/filter/LoginCheckFilter.java
+++ src/main/java/com/bonc/reggie/filter/LoginCheckFilter.java
@@ -1,6 +1,10 @@
 package com.bonc.reggie.filter;
 
+import com.alibaba.fastjson.JSON;
+import com.bonc.reggie.common.R;
+import com.google.gson.JsonObject;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.AntPathMatcher;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
@@ -14,11 +18,69 @@ import java.io.IOException;
 @WebFilter(filterName = "loginFilter",urlPatterns = "/*")
 @Slf4j
 public class LoginCheckFilter implements Filter {
+
+    // 路径匹配器，支持通配符
+    public  static final AntPathMatcher antPathMatcher = new AntPathMatcher();
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        /**
+         * 1、获取本次请求的URI
+         * 2、判断本次请求是否需要处理
+         * 3、如果不需要处理，则直接放行
+         * 4、判断登录状态，如果已登录，则直接放行
+         * 5、如果未登录则返回未登录结果
+         */
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
+
         log.info("拦截到请求: {}",request.getRequestURI());
-        filterChain.doFilter(request,response);
+
+        // 1、获取本次请求的URI
+        String requestURI = request.getRequestURI();
+
+        // 2、判断本次请求是否需要处理
+        String [] urls = new String[]{
+                "/employee/login",
+                "/employee/logout",
+                "/backend/**",
+                "/front/**"
+        };
+        boolean check = checkUrl(urls,requestURI);
+
+        // 3、如果不需要处理，则直接放行
+        if(check){
+            log.info("本次请求{}不需要处理",request.getRequestURI());
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        // 4、判断登录状态，如果已登录，则直接放行
+        if(request.getSession().getAttribute("employee")!=null){
+            log.info("用户已登录，用户id: {}",request.getSession().getAttribute("employee"));
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        // 5、如果未登录则返回未登录结果，通过输出流方式向客户端页面响应数据
+        log.info("用户未登录");
+        response.getWriter().write(JSON.toJSONString(R.error("NOTLOGIN")));
+
     }
+
+    /**
+     * 路径匹配，检查本次请求是否需要放行
+     * @param urls
+     * @param requestUrl
+     * @return
+     */
+    public boolean checkUrl(String[] urls,String requestUrl){
+        for (String url : urls) {
+            if(antPathMatcher.match(url,requestUrl)){
+                return true;
+            }
+        }
+        return false;
+    }
+
 }

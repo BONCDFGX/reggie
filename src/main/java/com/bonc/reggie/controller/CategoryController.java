5ff32aa693cf746dc6940164ec54cd12852e419c
Gy <1477056055@qq.com>
2023-09-20 17:27:24 +0800

    2023.09.20-23 CategoryController实现新增分类，分页查询功能

--- src/main/java/com/bonc/reggie/controller/CategoryController.java
+++ src/main/java/com/bonc/reggie/controller/CategoryController.java
@@ -1,9 +1,14 @@
 package com.bonc.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.bonc.reggie.common.R;
+import com.bonc.reggie.entity.Category;
 import com.bonc.reggie.service.CategoryService;
 import com.bonc.reggie.service.impl.CategoryServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,4 +21,38 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
+
+    /**
+     * 新增分类
+     * @param category
+     * @return
+     */
+    @RequestMapping
+    public R<String> save(@RequestBody Category category){
+        log.info("category: {}",category.toString());
+        categoryService.save(category);
+        return R.success("新增分类成功");
+    }
+
+
+    /**
+     * 分页查询
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    @RequestMapping("/page")
+    public R<Page> page(int page,int pageSize){
+        // 分页构造器
+        Page<Category> pageInfo = new Page<>(page,pageSize);
+        // 条件构造器
+        LambdaQueryWrapper<Category> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        // 添加排序条件，根据sort进行排序
+        lambdaQueryWrapper.orderByAsc(Category::getSort);
+
+        // 进行分页查询
+        categoryService.page(pageInfo,lambdaQueryWrapper);
+        return R.success(pageInfo);
+    }
+
 }

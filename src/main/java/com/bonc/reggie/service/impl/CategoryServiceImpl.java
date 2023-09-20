405ab419e99ed00a1363ff1bc99c199d44db5d21
Gy <1477056055@qq.com>
2023-09-20 11:53:07 +0800

    2023.09.20-22 搭建分类管理骨架，编写实体类，mapper，service和controller

---
+++ src/main/java/com/bonc/reggie/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,11 @@
+package com.bonc.reggie.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.bonc.reggie.entity.Category;
+import com.bonc.reggie.mapper.CategoryMapper;
+import com.bonc.reggie.service.CategoryService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> implements CategoryService {
+}

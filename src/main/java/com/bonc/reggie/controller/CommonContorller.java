0a89e295ca2d7fc4bd9454df7a3f252e7b0fe6f9
bonc <boncdfgx@outlook.com>
2023-09-22 00:09:10 +0800

    2023.09.22-29 CommonController实现文件下载功能

--- src/main/java/com/bonc/reggie/controller/CommonContorller.java
+++ src/main/java/com/bonc/reggie/controller/CommonContorller.java
@@ -3,12 +3,17 @@ package com.bonc.reggie.controller;
 import com.bonc.reggie.common.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -58,5 +63,36 @@ public class CommonContorller {
         log.info(file.toString());
         return R.success(fileName);
     }
+
+
+    @GetMapping("/download")
+    public void download(String name, HttpServletResponse response){
+
+        try {
+            // 输入流，通过输入流读取文件内容
+            FileInputStream fileInputStream = new FileInputStream(new File(basePath+name));
+            // 输出流，通过输出流将文件写回浏览器，在浏览器展示图片
+            ServletOutputStream outputStream = response.getOutputStream();
+
+            // 设置响应文件类型
+            response.setContentType("image/jpeg");
+
+            int len = 0;
+            byte bytes[] = new byte[1024];
+            while ((len=fileInputStream.read(bytes))!=-1){
+                outputStream.write(bytes,0,len);
+                outputStream.flush();
+            }
+
+            // 关闭资源
+            outputStream.close();
+            fileInputStream.close();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
 
 }

7025ccd8ee466d57fd0253c5a0eb6342f6012db4
bonc <boncdfgx@outlook.com>
2023-09-22 10:19:16 +0800

    2023.09.22-31 CategoryController实现根据条件查询分类数据功能

--- src/main/java/com/bonc/reggie/controller/CategoryController.java
+++ src/main/java/com/bonc/reggie/controller/CategoryController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * 分类管理
@@ -79,6 +80,26 @@ public class CategoryController {
         log.info(category.toString());
         categoryService.updateById(category);
         return R.success("分类信息修改成功");
+    }
+
+
+    /**
+     * 根据条件查询分类数据
+     * @param category
+     * @return
+     */
+    @GetMapping("/list")
+    public R<List<Category>> getList(@RequestBody Category category){
+        // 条件构造器
+        LambdaQueryWrapper<Category> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        // 添加条件
+        lambdaQueryWrapper.eq(category.getType()!=null,Category::getType,category.getType());
+        // 添加排序条件
+        lambdaQueryWrapper.orderByAsc(Category::getSort).orderByDesc(Category::getUpdateTime);
+
+        List<Category> list = categoryService.list(lambdaQueryWrapper);
+
+        return R.success(list);
     }
 
 }

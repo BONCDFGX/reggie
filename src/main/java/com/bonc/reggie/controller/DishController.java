cf0caa21fd3d1a02657d2365d50ae4dee0182430
bonc <boncdfgx@outlook.com>
2023-09-23 10:57:07 +0800

    2023.09.23-33 DishController实现菜品信息分页查询功能

--- src/main/java/com/bonc/reggie/controller/DishController.java
+++ src/main/java/com/bonc/reggie/controller/DishController.java
@@ -1,14 +1,20 @@
 package com.bonc.reggie.controller;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.bonc.reggie.common.R;
 import com.bonc.reggie.dto.DishDto;
+import com.bonc.reggie.entity.Category;
+import com.bonc.reggie.entity.Dish;
+import com.bonc.reggie.service.CategoryService;
 import com.bonc.reggie.service.DishFlavorService;
 import com.bonc.reggie.service.DishService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 菜品管理
@@ -24,6 +30,9 @@ public class DishController {
     @Autowired
     DishFlavorService dishFlavorService;
 
+    @Autowired
+    CategoryService categoryService;
+
     /**
      * 新增菜品
      * @param dishDto
@@ -34,6 +43,52 @@ public class DishController {
         log.info(dishDto.toString());
         dishService.saveWithFlavor(dishDto);
         return R.success("新增菜品成功");
+    }
+
+
+    /**
+     * 菜品信息分页查询
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page,int pageSize,String name){
+
+        // 构造分页构造器
+        Page<Dish> pageInfo = new Page<>(page,pageSize);
+        Page<DishDto> dishDtoPage = new Page<>(page,pageSize);
+
+        // 条件构造器
+        LambdaQueryWrapper<Dish> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        // 添加过滤条件
+        lambdaQueryWrapper.like(name!=null,Dish::getName,name);
+        // 添加排序条件
+        lambdaQueryWrapper.orderByDesc(Dish::getUpdateTime);
+
+        //执行分页查询
+        dishService.page(pageInfo,lambdaQueryWrapper);
+
+        List<DishDto> list = pageInfo.getRecords().stream().map((item) -> {
+            DishDto dishDto = new DishDto();
+            BeanUtils.copyProperties(item, dishDto);
+            Long categoryId = item.getCategoryId();// 分类id
+            // 根据id查询分类对象
+            Category category = categoryService.getById(categoryId);
+            if(category!=null){
+                String categoryName = category.getName();
+                dishDto.setCategoryName(categoryName);
+            }
+            return dishDto;
+        }).collect(Collectors.toList());
+
+        // 对象拷贝,忽略records
+        BeanUtils.copyProperties(pageInfo,dishDtoPage,"records");
+
+        dishDtoPage.setRecords(list);
+
+        return R.success(dishDtoPage);
     }
 
 }

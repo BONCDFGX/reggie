0dc883975a9a8dec86674dfe16116f0900dcad55
bonc <boncdfgx@outlook.com>
2023-09-24 22:58:40 +0800

    2023.09.24-54 ShoppingCartController实现添加购物车功能

--- src/main/java/com/bonc/reggie/controller/ShoppingCartController.java
+++ src/main/java/com/bonc/reggie/controller/ShoppingCartController.java
@@ -1,6 +1,14 @@
 package com.bonc.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.bonc.reggie.common.BaseContext;
+import com.bonc.reggie.common.R;
+import com.bonc.reggie.entity.ShoppingCart;
+import com.bonc.reggie.service.ShoppingCartService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +16,54 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/shoppingCart")
 @Slf4j
 public class ShoppingCartController {
+
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+
+    /**
+     * 添加购物车
+     * @param shoppingCart
+     * @return
+     */
+    @PostMapping("/add")
+    public R<ShoppingCart> add(@RequestBody ShoppingCart shoppingCart){
+        log.info("购物车数据: {}",shoppingCart);
+
+        // 设置用户id，指定当前是哪个用户的购物车数据
+        Long currentId = BaseContext.getCurrentId();
+        shoppingCart.setUserId(currentId);
+
+        LambdaQueryWrapper<ShoppingCart> shoppingCartLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        shoppingCartLambdaQueryWrapper.eq(ShoppingCart::getUserId,currentId);
+
+        // 查询当前菜品或套餐是否已在购物车中
+        Long dishId = shoppingCart.getDishId();
+        if(dishId!=null){
+            // 添加到购物车的是菜品
+            shoppingCartLambdaQueryWrapper.eq(ShoppingCart::getDishId,dishId);
+        }else {
+            // 添加到购物车的是套餐
+            shoppingCartLambdaQueryWrapper.eq(ShoppingCart::getSetmealId,shoppingCart.getSetmealId());
+        }
+
+        // select * from shopping_cart where user_id = ? and dish_id/setmeal_id = ?
+        ShoppingCart cartServiceOne = shoppingCartService.getOne(shoppingCartLambdaQueryWrapper);
+
+        if(cartServiceOne!=null){
+            // 如果已经存在，就在原来数量基础上加一
+            Integer number = cartServiceOne.getNumber();
+            cartServiceOne.setNumber(number+1);
+            shoppingCartService.updateById(cartServiceOne);
+        }else {
+            // 如果不存在，则添加到购物车，数量默认为一
+            shoppingCart.setNumber(1);
+            shoppingCartService.save(shoppingCart);
+            cartServiceOne = shoppingCart;
+        }
+
+        return R.success(cartServiceOne);
+
+    }
+
 }

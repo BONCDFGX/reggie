41d2c4688c71c4cf9c6b15b242f5da5f65937db1
bonc <boncdfgx@outlook.com>
2023-09-24 12:16:34 +0800

    2023.09.24-44 LoginCheckFilter过滤器放行移动段发送短信和移动端登录请求

--- src/main/java/com/bonc/reggie/filter/LoginCheckFilter.java
+++ src/main/java/com/bonc/reggie/filter/LoginCheckFilter.java
@@ -45,7 +45,10 @@ public class LoginCheckFilter implements Filter {
                 "/employee/login",
                 "/employee/logout",
                 "/backend/**",
-                "/front/**"
+                "/front/**",
+                "/common/**",
+                "/user/sendMsg",
+                "/user/login"
         };
         boolean check = checkUrl(urls,requestURI);
 
@@ -56,11 +59,20 @@ public class LoginCheckFilter implements Filter {
             return;
         }
 
-        // 4、判断登录状态，如果已登录，则直接放行
+        // 4-1、判断登录状态，如果已登录，则直接放行
         if(request.getSession().getAttribute("employee")!=null){
             log.info("用户已登录，用户id: {}",request.getSession().getAttribute("employee"));
             Long empId = (Long) request.getSession().getAttribute("employee");
             BaseContext.setCurrentId(empId);
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        // 4-2、判断登录状态，如果已登录，则直接放行
+        if(request.getSession().getAttribute("user")!=null){
+            log.info("用户已登录，用户id: {}",request.getSession().getAttribute("user"));
+            Long userId = (Long) request.getSession().getAttribute("user");
+            BaseContext.setCurrentId(userId);
             filterChain.doFilter(request,response);
             return;
         }
